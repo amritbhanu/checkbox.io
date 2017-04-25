729d8fbb73f228b9cb3a9f0dc55e85e5bb2df321
amrit <amrit@amrit.local>
2017-04-25 01:39:22 -0400

    save

--- server-side/site/proxy.js
+++ server-side/site/proxy.js
@@ -2,10 +2,43 @@ var redis = require('redis')
 var multer  = require('multer')
 var http      = require('http');
 var httpProxy = require('http-proxy');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 var client = redis.createClient(6379, "34.210.23.153", {})
 
-var ports = {};
-var proxy   = httpProxy.createProxyServer(ports);
+var proxy   = httpProxy.createProxyServer();
+
+http.createServer(function (req, res) {
+
+    loadBalance (req, res)
+
+}).listen(5000);
+
+function loadBalance (req, res) {
+
+    client.rpoplpush('proxy','proxy', function(err, reply)
+    {
+        client.lindex('proxy', 0, function(err, ip)
+        {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "GET", 'http://' + ip + ':3003/stats', false ); // false for synchronous request
+            xmlHttp.send( null );
+            console.log(parseInt(xmlHttp.responseText));
+            if (parseInt(xmlHttp.responseText) <= 15) {
+                target = 'http://' + ip+ ':3003';
+                console.log('target is: %s', target);
+                proxy.web(req, res,
+                {
+                    target: target
+                });
+            } else {
+                console.log (ip + ' is on heavy load');
+                loadBalance (req, res);
+            }
+        });
+    });
+}
+
 var server1  = http.createServer(function(req, res)
 { 
   client.rpoplpush('proxy','proxy',function(err,value) 
@@ -17,3 +50,12 @@ var server1  = http.createServer(function(req, res)
 });
 console.log("Proxy server listening on port: 3000");
 server1.listen(3000)
+
+
+//
+// Create your server that makes an operation that waits a while
+// and then proxies the request
+//
+
+
+
--- server-side/site/server.js
+++ server-side/site/server.js
@@ -11,6 +11,7 @@ var express = require('express'),
 var app = express()
 var redis = require('redis')
 var client = redis.createClient(6379, "34.210.23.153", {})
+var os = require('os');
 
 var fs = require('fs'),
     readline = require('readline');
@@ -94,6 +95,50 @@ app.get('/hiddenFeature', function(req, res){
 			else res.status(200).send('Sorry! This feature is not available.');
 	});
 });
+
+
+function cpuTicksAcrossCores() 
+{
+  //Initialise sum of idle and time of cores and fetch CPU info
+  var totalIdle = 0, totalTick = 0;
+  var cpus = os.cpus();
+ 
+  //Loop through CPU cores
+  for(var i = 0, len = cpus.length; i < len; i++) 
+  {
+		//Select CPU core
+		var cpu = cpus[i];
+		//Total up the time in the cores tick
+		for(type in cpu.times) 
+		{
+			totalTick += cpu.times[type];
+		}     
+		//Total up the idle time of the core
+		totalIdle += cpu.times.idle;
+  }
+ 
+  //Return the average Idle and Tick times
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
+
+var startMeasure = cpuTicksAcrossCores();
+
+function cpuAverage()
+{
+	var endMeasure = cpuTicksAcrossCores(); 
+ 
+	//Calculate the difference in idle and total time between the measures
+	var idleDifference = endMeasure.idle - startMeasure.idle;
+	var totalDifference = endMeasure.total - startMeasure.total;
+ 
+	//Calculate the average percentage CPU usage
+	return (((totalDifference - idleDifference) / totalDifference) * 100);
+}
+
+app.get('/stats', function(req, res) {
+    cpu = Math.round(cpuAverage());
+    res.send(cpu.toString());
+})
 
 // app.listen(PORT);
 //    console.log('Listening on port 3003...');
