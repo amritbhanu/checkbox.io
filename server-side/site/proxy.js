331bb893d217e3b7993854b2ec891e59b597516b
amritbhanu <amritbhanu@gmail.com>
2017-04-25 01:42:34 -0400

    save

--- server-side/site/proxy.js
+++ server-side/site/proxy.js
@@ -8,11 +8,16 @@ var client = redis.createClient(6379, "34.210.23.153", {})
 
 var proxy   = httpProxy.createProxyServer();
 
+//
+// Create your server that makes an operation that waits a while
+// and then proxies the request
+//
+
 http.createServer(function (req, res) {
 
     loadBalance (req, res)
 
-}).listen(5000);
+}).listen(3000);
 
 function loadBalance (req, res) {
 
@@ -39,23 +44,20 @@ function loadBalance (req, res) {
     });
 }
 
-var server1  = http.createServer(function(req, res)
-{ 
-  client.rpoplpush('proxy','proxy',function(err,value) 
-  {
-    if (err) throw err;
-    proxy.web( req, res, {target: "http://0.0.0.0:3003" } );
-    console.log("Redirecting to ip :"+value)
-  })
-});
-console.log("Proxy server listening on port: 3000");
-server1.listen(3000)
+// var server1  = http.createServer(function(req, res)
+// { 
+//   client.rpoplpush('proxy','proxy',function(err,value) 
+//   {
+//     if (err) throw err;
+//     proxy.web( req, res, {target: "http://0.0.0.0:3003" } );
+//     console.log("Redirecting to ip :"+value)
+//   })
+// });
+// console.log("Proxy server listening on port: 3000");
+// server1.listen(3000)
 
 
-//
-// Create your server that makes an operation that waits a while
-// and then proxies the request
-//
+
 
 
 
